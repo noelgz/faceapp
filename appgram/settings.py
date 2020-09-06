13a59caebf00ebe03e7dc21dd1c626ef54f28429
Noel Armando Gonzalez <51343660+noelgz@users.noreply.github.com>
2020-09-06 17:08:47 -0300

    Update settings.py

--- appgram/settings.py
+++ appgram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'klz)i0=l^m6zv6muh0-wg=z%aoyb)$psgxyqqwye%%p6kv1!wf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
